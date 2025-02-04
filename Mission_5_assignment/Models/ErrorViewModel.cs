017cb9e08cc97912070666e45f15f50e238c025c
trey1144 <trey1144@hotmail.com>
2025-02-03 18:33:06 -0700

    Add project files.

---
+++ Mission_5_assignment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mission_5_assignment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Mission_5_assignment/wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿
+$("#btnSubmit").click(function () {
+    let iHours = $("#numHours").val();
+
+    if (iHours < 1) {
+        alert("Please enter a number greater than 1 to see calculated total!");
+        return;
+    }
+
+    let iRate = $("#numRate").val();
+    let iTotal = iHours * iRate;
+    $("#txtTotal").val("$" + iTotal);
+})
